5bac64bfbaa0c312f4b531808741aa3775b1678f
lovedream Anuruttikun <lovedream.bs@gmail.com>
2026-02-15 00:41:27 +0700

    env

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
+const apiProxyBaseUrl = process.env.API_PROXY_BASE_URL || "http://localhost:8080";
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8080/api/:path*",
+        destination: `${apiProxyBaseUrl}/api/:path*`,
       },
     ];
   },
